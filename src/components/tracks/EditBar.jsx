9fb616ea09bb63b58875531cb02a6663b2b23e1e
fmj23 <fiedlejo@oregonstate.edu>
2021-11-25 01:09:56 -0700

    Added a rudimentary slider
    Slider steps from -10 to 10 in 0.5 increments. The value is returned to CreateTrackContainer and accessible via the 'pitchValue' variable

---
+++ src/components/tracks/EditBar.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import Slider from '@mui/material/Slider';
+import '../../styles/pageStyle.css';
+
+const EditBar = (props) => {
+
+    const {getPitchValueFromBar} = props;
+
+    const [pitchValue, setPitch] = useState(null);
+
+    const marks = [
+        {
+            value: -10,
+            label: '-10',
+        },
+        {
+            value: -5,
+            label: '-5',
+        },
+        {
+          value: 0,
+          label: '0',
+        },
+        {
+          value: 5,
+          label: '5',
+        },
+        {
+          value: 10,
+          label: '10',
+        },
+      ];
+
+    function valuetext(value) {
+        console.log("Slider:", value)
+        setPitch(value);
+        return `${value}`;
+    }
+    
+    return (
+        <div class="editBar">
+            EditBar
+            <Slider
+                aria-label= "Steps"
+                defaultValue={0.0}
+                step={0.5}
+                min={-10.0}
+                max={10.0}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+                marks={marks}
+            />
+            {getPitchValueFromBar(pitchValue)}
+        </div>
+    )
+}
+
+export default EditBar
